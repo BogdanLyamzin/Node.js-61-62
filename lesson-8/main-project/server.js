cfb3812a29b9133bcbf9ab6cd2cd56a12dd100c8
unknown <bogdan.lyamzin.d@gmail.com>
2023-04-12 21:41:46 +0300

    finish lesson 8

---
+++ lesson-8/main-project/server.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+// require("dotenv").config();
+
+const app = require('./app');
+
+const { DB_HOST, PORT = 3000 } = process.env;
+
+mongoose.connect(DB_HOST)
+  .then(() => app.listen(PORT))
+  .catch(error => console.log(error.message));
+
+
